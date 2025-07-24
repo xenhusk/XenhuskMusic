a3f1fbb482d068ecca72e08dd882ccc929e6e049
mardous <mardous.contact@gmail.com>
2025-07-24 15:21:20 -0400

    fix(lyrics): add user agent and album name to LrcLibApi requests

--- app/src/main/java/com/mardous/booming/http/lyrics/lrclib/LrcLibApi.kt
+++ app/src/main/java/com/mardous/booming/http/lyrics/lrclib/LrcLibApi.kt
@@ -1,5 +1,6 @@
 package com.mardous.booming.http.lyrics.lrclib
 
+import com.mardous.booming.BuildConfig
 import com.mardous.booming.http.lyrics.LyricsApi
 import com.mardous.booming.model.DownloadedLyrics
 import com.mardous.booming.model.Song
@@ -7,12 +8,15 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.http.encodeURLParameter
+import io.ktor.http.userAgent
 
 class LrcLibApi(private val client: HttpClient) : LyricsApi {
 
     override suspend fun songLyrics(song: Song, title: String, artist: String): DownloadedLyrics? {
         val lyrics = client.get("https://lrclib.net/api/search") {
+            userAgent("Booming Music v${BuildConfig.VERSION_NAME} (https://github.com/mardous/BoomingMusic)")
             url.encodedParameters.append("q", "$artist $title".encodeURLParameter())
+            url.encodedParameters.append("album_name", song.albumName.encodeURLParameter())
         }.body<List<DownloadedLyrics>>()
         if (lyrics.isEmpty()) {
             return null

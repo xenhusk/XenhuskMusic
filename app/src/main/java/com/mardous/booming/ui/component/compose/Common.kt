52d6ae40d5205ff62abc7c0de175fbc5a78a5df9
mardous <mardous.contact@gmail.com>
2025-09-01 17:22:21 -0400

    refactor(ui): remove unused CollapsingTopAppBar

--- app/src/main/java/com/mardous/booming/ui/component/compose/Common.kt
+++ app/src/main/java/com/mardous/booming/ui/component/compose/Common.kt
@@ -17,56 +17,20 @@
 
 package com.mardous.booming.ui.component.compose
 
-import androidx.appcompat.R
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CollapsingTopAppBar(
-    title: String,
-    isScrollable: Boolean,
-    onBackClick: () -> Unit,
-) {
-    val scrollBehavior = if (isScrollable) {
-        TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-    } else {
-        null
-    }
-    val upButton = IconButton(onClick = onBackClick) {
-        Icon(
-            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-            contentDescription = stringResource(R.string.abc_action_bar_up_description)
-        )
-    }
-    if (scrollBehavior != null) {
-        LargeTopAppBar(
-            title = { Text(title) },
-            navigationIcon = { upButton },
-            scrollBehavior = scrollBehavior
-        )
-    } else {
-        TopAppBar(
-            title = { Text(title) },
-            navigationIcon = { upButton }
-        )
-    }
-}
 
 @Composable
 fun ActionButton(
